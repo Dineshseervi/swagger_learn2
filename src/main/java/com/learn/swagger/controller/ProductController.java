953803632ce09f841a001cbeafb61cb63e0f716b
Dinesh Seervi <dineshseervi@fico.com>
2022-08-04 16:19:12 +0530

    Added new method and swagger doc annotation

--- src/main/java/com/learn/swagger/controller/ProductController.java
+++ src/main/java/com/learn/swagger/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.learn.swagger.controller;
 
+import com.learn.swagger.model.Donor;
 import com.learn.swagger.model.Product;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.learn.swagger.service.IProductService;
 
 import java.util.List;
@@ -41,5 +44,29 @@ public class ProductController {
     public void addProduct(@RequestBody Product product)
     {
         productService.addProduct(product);
+    }
+
+
+
+    @PostMapping
+    @ApiOperation(value = "Create a new donor", response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created a new donor"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Application failed to process the request")
+    }
+    )
+    public ResponseEntity<Donor> createDonor(@RequestBody Donor donor){
+            //Donor savedDonor = donorRepository.save(donor);
+            return new ResponseEntity<Donor>(donor, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping(value ="/product/{id}")
+    @ApiOperation(value = "Deletes specific donor with the supplied donor id")
+    public void delete(@PathVariable("id") Long id){
+        //TODO
     }
 }
